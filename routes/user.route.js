0a1ee6dee86fe41b2a694b909ef8ee28466ef1fa
Alberto Velázquez Rapado <86166779+AlVelRap@users.noreply.github.com>
2023-07-19 21:20:47 +0200

    feat: add authentication required to routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,15 +1,14 @@
 module.exports = (app) => {
-    const { VERSION, REST, API } = require("../constants");
-    const user = require("../controllers/user.controller");
-  
-    var router = require("express").Router();
+  const { isAuthenticated } = require("../auth");
+  const { VERSION, REST, API } = require("../constants");
+  const user = require("../controllers/user.controller");
+  var router = require("express").Router();
 
-    router.post("/register", user.register);
-    router.post("/login", user.login);
-    router.get("/", user.findOne);
-    router.put("/", user.update);
-    router.delete("/", user.delete);
-  
-    app.use(`/${API}/${REST}/${VERSION}/user`, router);
-  };
-  
+  router.post("/register", user.register);
+  router.post("/login", user.login);
+  router.get("/", isAuthenticated, user.findOne);
+  router.put("/", isAuthenticated, user.update);
+  router.delete("/", isAuthenticated, user.delete);
+
+  app.use(`/${API}/${REST}/${VERSION}/user`, router);
+};
