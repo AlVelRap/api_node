35d01b3a551cd572b5a91e9ad8f2009705a0ee35
Alberto Velázquez Rapado <86166779+AlVelRap@users.noreply.github.com>
2024-03-04 18:42:42 +0100

    refactor: remove comments and unused elements

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,31 +1,9 @@
 // -----------------------
 // TODO:
 //  - Update only pass
-//  - Move Register and Login data access logic to the service
 // -----------------------
 
-const User = require("../models/user.model.js");
 const userService = require("../services/user.service.js");
-const crypto = require("crypto");
-const jwt = require("jsonwebtoken");
-const {
-  JWT_SECRET,
-  DIGEST_PASSWORD,
-  JWT_LIFE,
-  JWT_ALGORITHM,
-} = require("../constants");
-
-// Constants for encrypt password
-const iterations = 10000;
-const keylen = 64;
-
-// Function for token
-const signToken = (_id) => {
-  return jwt.sign({ _id }, JWT_SECRET, {
-    algorithm: JWT_ALGORITHM,
-    expiresIn: JWT_LIFE,
-  });
-};
 
 exports.register = (req, res) => {
   if (!req.body) {
@@ -49,47 +27,6 @@ exports.register = (req, res) => {
     };
     return res.send(userResponse);
   });
-  // userService.findByEmail(req.body.email, (err, data) => {
-  //   if (data) {
-  //     return res.status(400).send({
-  //       // Check this HTTP Code
-  //       message: "User already exists.",
-  //     });
-  //     return res.send(data);
-  //   }
-  //   // Create salt
-  //   crypto.randomBytes(16, (err, saltBytes) => {
-  //     const newSalt = saltBytes.toString("base64");
-  //     // Encrypt password
-  //     crypto.pbkdf2(
-  //       req.body.password,
-  //       newSalt,
-  //       iterations,
-  //       keylen,
-  //       DIGEST_PASSWORD,
-  //       (err, key) => {
-  //         const encryptedPassword = key.toString("base64");
-
-  //         const user = {
-  //           name: req.body.name,
-  //           lastname: req.body.lastname,
-  //           email: req.body.email,
-  //           password: encryptedPassword,
-  //           salt: newSalt,
-  //         };
-
-  //         userService.create(user, (err, data) => {
-  //           if (err)
-  //             return res.status(500).send({
-  //               message:
-  //                 err.message || "An error occurred while creating the user.",
-  //             });
-  //           else res.send(data);
-  //         });
-  //       }
-  //     );
-  //   });
-  // });
 };
 
 exports.login = (req, res) => {
@@ -105,31 +42,6 @@ exports.login = (req, res) => {
     }
     return res.send(data);
   });
-
-  // userService.findByEmail(req.body.email, (err, data) => {
-  //   if (!data) {
-  //     return res.status(404).send({
-  //       message: `Incorrect username or password`,
-  //     });
-  //   }
-  //   crypto.pbkdf2(
-  //     req.body.password,
-  //     data.salt,
-  //     iterations,
-  //     keylen,
-  //     DIGEST_PASSWORD,
-  //     (err, key) => {
-  //       const encryptedPassword = key.toString("base64");
-  //       if (data.password === encryptedPassword) {
-  //         const token = signToken(data.id_user);
-  //         return res.send({ token });
-  //       }
-  //       return res.status(404).send({
-  //         message: `Incorrect username or password`,
-  //       });
-  //     }
-  //   );
-  // });
 };
 
 exports.findOne = (req, res) => {
