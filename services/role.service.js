bce3f71f7600c091f304bc1c92c3945693e75a6f
Alberto Velázquez Rapado <86166779+AlVelRap@users.noreply.github.com>
2024-03-06 19:12:36 +0100

    feat: create role service

---
+++ services/role.service.js
@@ -0,0 +1,87 @@
+const db = require("../models");
+const Role = db.role;
+const Op = db.Sequelize.Op;
+
+// TODO:
+// - Add exceptions for ResourceNotFoundException
+
+exports.create = (newRole, result) => {
+  Role.create(newRole)
+    .then((data) => {
+      console.log("role created: ", { ...data.dataValues });
+      result(null, { ...data.dataValues });
+    })
+    .catch((err) => {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    });
+};
+
+exports.findById = (id, result) => {
+  Role.findByPk(id)
+    .then((data) => {
+      if (data) {
+        console.log("role found: ", data.dataValues);
+        result(null, { ...data.dataValues });
+        return;
+      }
+      result({ kind: "not_found" }, null);
+    })
+    .catch((err) => {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    });
+};
+
+exports.findByName = (name, result) => {
+  Role.findOne({ where: { name: name } })
+    .then((data) => {
+      if (data) {
+        console.log("role found: ", data.dataValues);
+        result(null, { ...data.dataValues });
+        return;
+      }
+      result({ kind: "not_found" }, null);
+    })
+    .catch((err) => {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    });
+};
+
+exports.updateById = (id, role, result) => {
+  Role.update(role, { where: { id_role: id } })
+    .then((num) => {
+      if (num == 0) {
+        result({ kind: "not_found" }, null);
+        return;
+      }
+      console.log("role updated: ", { id_role: id, ...role });
+      result(null, { id_role: id, ...role });
+    })
+    .catch((err) => {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    });
+};
+
+exports.remove = (id, result) => {
+  Role.destroy({ where: { id_role: id } })
+    .then((num) => {
+      if (num == 0) {
+        result({ kind: "not_found" }, null);
+        return;
+      }
+      console.log("Erased role with id: ", id);
+      result(null, num);
+    })
+    .catch((err) => {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    });
+};
