5008aa71271c9c4a44d7190bf9b3dd6f1c02f6e5
Alberto Velázquez Rapado <86166779+AlVelRap@users.noreply.github.com>
2024-06-28 19:03:11 +0200

    feat: add id_user to user creation

--- init/index.js
+++ init/index.js
@@ -2,24 +2,28 @@ const userService = require("../services/user.service");
 const roleService = require("../services/role.service");
 exports.runs = async () => {
   const user1 = {
+    id_user: "test-alberto",
     name: "Alberto",
     lastname: "Velázquez Rapado",
     email: "alberto@dominio.com",
     password: "prueba",
   };
   const user2 = {
+    id_user: "test-paco",
     name: "Paco",
     lastname: "Fernández Carrión",
     email: "paco@dominio.com",
     password: "paco",
   };
   const user3 = {
+    id_user: "test-antonio",
     name: "Antonio",
     lastname: "Gómez García",
     email: "antonio@dominio.com",
     password: "antonio",
   };
   const user4 = {
+    id_user: "test-juan",
     name: "Juan",
     lastname: "Hernandez Salamanqués",
     email: "juan@dominio.com",
