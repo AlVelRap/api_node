3c4e0e8fb09b8b5d6d63ebd7eed60084567f4bd1
Alberto Velázquez Rapado <86166779+AlVelRap@users.noreply.github.com>
2024-06-29 19:55:34 +0200

    feat(route): add role and auth check for role routes

--- routes/role.route.js
+++ routes/role.route.js
@@ -1,18 +1,16 @@
 module.exports = (app) => {
-  const { isAuthenticated } = require("../auth");
+  const { isAuthenticated, hasRoles } = require("../auth");
   const { VERSION, REST, API } = require("../constants");
   const role = require("../controllers/role.controller");
   var router = require("express").Router();
 
-  // DONT FORGET TO UNCOMMENT isAuthenticated AGAIN!!!!!
-
-  router.post("/",/*isAuthenticated,*/ role.create);
-  router.post("/user",/*isAuthenticated,*/ role.addUser);
-  router.get("/", /*isAuthenticated,*/ role.findAll);
-  router.get("/:id_role", /*isAuthenticated,*/ role.findOne);
-  router.put("/:id_role",/*isAuthenticated,*/ role.update);
-  router.delete("/:id_role",/*isAuthenticated,*/ role.delete);
-  router.delete("/",/*isAuthenticated,*/ role.deleteAll);
+  router.post("/", isAuthenticated, hasRoles(["admin"]), role.create);
+  router.post("/user", isAuthenticated, hasRoles(["admin"]), role.addUser);
+  router.get("/", isAuthenticated, hasRoles(["admin"]), role.findAll);
+  router.get("/:id_role", isAuthenticated, hasRoles(["admin"]), role.findOne);
+  router.put("/:id_role", isAuthenticated, hasRoles(["admin"]), role.update);
+  router.delete("/:id_role", isAuthenticated, hasRoles(["admin"]), role.delete);
+  router.delete("/", isAuthenticated, hasRoles(["admin"]), role.deleteAll);
 
   app.use(`/${API}/${REST}/${VERSION}/role`, router);
 };
